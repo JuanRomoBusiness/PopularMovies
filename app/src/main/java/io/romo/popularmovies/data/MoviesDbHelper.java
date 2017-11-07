97617b0e5d376f5c1ae5ea1ea965d501d273a9da
Juan Romo <juanromobusiness@gmail.com>
2017-11-06 21:28:25 -0600

    SQLite database implemented

---
+++ app/src/main/java/io/romo/popularmovies/data/MoviesDbHelper.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 Juan Romo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.romo.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class MoviesDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "movies.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public MoviesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " +
+                MoviesContract.MovieEntry.TABLE_NAME + " (" +
+                MoviesContract.MovieEntry._ID + " INTEGER PRIMARY KEY," +
+                MoviesContract.MovieEntry.COLUMN_TITLE + " TEXT NOT NULL" +
+                ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MoviesContract.MovieEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
